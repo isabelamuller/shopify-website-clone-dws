e26175bc26e6bccb309e1367d0a1740c256bc389
Isabela Müller <isabela.muller-ext@dentsu.com>
2022-12-07 11:32:18 -0300

    added side nav to mobile

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 
-const dropdownActive = () => {
-   let click = document.getElementById("dropdown");
-   let menuTitle = document.getElementById("dropdown-btn")
-   if (click.style.display === "none") {
-      click.style.display = "block";
-      menuTitle.style.fontWeight = "600";
-   } else {
-      click.style.display = "none";
-      menuTitle.style.fontWeight = "100"
-   }
-}
+const dropdownBtnPc = document.getElementById("dropdown-btn");
+const dropdownPc = document.getElementById("dropdown");
+
+dropdownBtnPc.addEventListener('click', () => {
+   dropdownPc.classList.toggle('open');
+})
+
+
 
 const modeSwitcher = () => {
    let bodyElement = document.body;
    bodyElement.classList.toggle("dark-mode");
 
 }
-
 
 let popUp = document.getElementById("search-bar-popup");
 let searchBar = document.getElementById("search-bar-container");
@@ -40,58 +36,32 @@ window.onclick = (event) => {
    }
 }
 
-// const searchBar = document.querySelector(".search-bar-container");
-// const popUp = document.querySelector(".search-bar-popup");
 
-// searchBar.addEventListener('click', () => {
-//    popUp.classList.toggle('show');
-// });
-// document.addEventListener('click', ({
-//    target
-// }) => {
-//    if (target.matches('.search-bar-container') === false) {
-//       popUp.classList.remove('show');
-//    }
-// });
+const sideNav = document.getElementsByClassName("side-navbar-mobile")[0];
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const pcNav = document.getElementById("navbar");
+const closeBtn = document.getElementById("close-btn-side-nav");
 
 
-// $("#search-bar-container").click(function() {
-//    $("#search-bar-popup").toggle("show");
-// })
-
-
-function openSideNav() {
-   let sideNavElement = document.getElementById("side-nav");
-   let teste = document.getElementById("side-nav-mobile-top");
-   let pcNav = document.getElementById("navbar");
-   let teste2 = document.getElementById('side-nav-menu-container');
-   let teste3 = document.body;
-   console.log(teste)
-   teste2.style.display = 'flex';
-   sideNavElement.style.width = "100vw";
-   sideNavElement.style.height = "100vh";
-   teste.style.display = "flex";
-   teste.style.width = "100vw";
-
-   sideNavElement.style.display = "inline-block";
-   teste3.style.overflow = "hidden";
-   // isso ta uma bagunca dsclp KKK
-}
-
-
-const closeSideNav = () => {
-   let sideNavElement = document.getElementById("side-nav");
-   let teste = document.getElementById("side-nav-mobile-top");
-   let pcNav = document.getElementById("navbar");
-   let teste2 = document.getElementById('side-nav-menu-container');
+hamburgerIcon.addEventListener('click', () => {
+   sideNav.classList.toggle('open');
    
-   teste.style.display = 'none';
-   teste2.style.display = 'none';
-   sideNavElement.style.width = "0";
-   sideNavElement.style.height = "0";
-   pcNav.style.display = "flex"
-   teste3.style.overflow = "scroll";
-}
+   pcNav.classList.toggle('close');
+   document.body.style.overflow = "hidden"
+});
 
+closeBtn.addEventListener('click', () => {
+   sideNav.classList.remove('open');
+   pcNav.classList.remove('close');
+   document.body.style.overflow = "scroll"
+})
 
+const menuBtnSideNav = document.getElementById("side-nav-menu-wrapper");
+const menuArrow = document.getElementById("arrow-side-nav")
+const dropdownMenuSideNav = document.getElementById("side-nav-api-dropdown-container");
 
+menuBtnSideNav.addEventListener('click', () => {
+   dropdownMenuSideNav.classList.toggle('open');
+   menuArrow.classList.toggle("open");
+   menuBtnSideNav.classList.toggle('open');
+})
